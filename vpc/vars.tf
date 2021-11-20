2ebcf4a58014c1afec2304599aa81101f8d05391
Chandra Ummadi <ummadi.chandra@gmail.com>
2021-11-20 10:30:05 +0530

    vpc updated subnets

--- vpc/vars.tf
+++ vpc/vars.tf
@@ -1,9 +1,9 @@
 variable "ENV" {}
 variable "VPC_CIDR_MAIN" {}
 variable "VPC_CIDR_ADDON" {}
-#variable "PRIVATE_SUBNETS" {}
-#variable "PUBLIC_SUBNETS" {}
-#variable "AZS" {}
-#variable "DEFAULT_VPC_ID" {}
-#variable "DEFAULT_VPC_CIDR" {}
-#variable "INTERNAL_HOSTEDZONE_ID" {}
+variable "PRIVATE_SUBNETS" {}
+variable "PUBLIC_SUBNETS" {}
+variable "AZS" {}
+variable "DEFAULT_VPC_ID" {}
+variable "DEFAULT_VPC_CIDR" {}
+variable "INTERNAL_HOSTEDZONE_ID" {}
