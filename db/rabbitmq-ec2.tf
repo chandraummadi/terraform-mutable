a76f3db8c63bca84c2701b8acc6ccd8d51a6a614
Chandra Ummadi <ummadi.chandra@gmail.com>
2021-11-28 20:40:01 +0530

    db with rabbitmq service

---
+++ db/rabbitmq-ec2.tf
@@ -0,0 +1,93 @@
+resource "aws_spot_instance_request" "rabbitmq" {
+  ami                    = data.aws_ami.ami.id
+  instance_type          = "t2.micro"
+  spot_type = "persistent"
+  instance_interruption_behavior = "stop"
+  vpc_security_group_ids = [aws_security_group.rabbitmq-sg.id]
+  subnet_id = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS_IDS[0]
+  wait_for_fulfillment   = true
+  tags = {
+    Name = "rabbitmq-${var.ENV}"
+  }
+}
+
+resource "aws_ec2_tag" "tags" {
+  resource_id = aws_spot_instance_request.rabbitmq.spot_instance_id
+  key         = "Name"
+  value       = "rabbitmq-${var.ENV}"
+}
+
+
+resource "aws_security_group" "rabbitmq-sg" {
+  name        = "rabbitmq-${var.ENV}"
+  description = "rabbitmq-${var.ENV}"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID
+
+  ingress = [
+    {
+      description      = "rabbitmq"
+      from_port        = 5672
+      to_port          = 5672
+      protocol         = "tcp"
+      cidr_blocks      = local.ALL_CIDR
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    },
+    {
+      description      = "ssh"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = local.ALL_CIDR
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
+
+  egress = [
+    {
+      description      = " outgoing "
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }]
+
+  tags = {
+    Name = "rabbitmq-${var.ENV}"
+  }
+}
+
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = data.terraform_remote_state.vpc.outputs.INTERNAL_HOSTEDZONE_ID
+  name    = "rabbitmq-${var.ENV}.roboshop.internal"
+  type    = "A"
+  ttl     = "300"
+  records = [aws_spot_instance_request.rabbitmq.private_ip]
+  allow_overwrite = true
+}
+
+resource "null_resource" "ansible" {
+  depends_on = [aws_route53_record.rabbitmq]
+  provisioner "remote-exec" {
+    connection {
+      host     = aws_spot_instance_request.rabbitmq.private_ip
+      user     = local.ssh_user
+      password = local.ssh_pass
+    }
+    inline = [
+      "sudo yum install python3-pip -y",
+      "sudo pip3 install pip --upgrade",
+      "sudo pip3 install ansible",
+      "ansible-pull -U https://github.com/chandraummadi/Devops-projects.git Ansible/roboshop/roboshop-pull.yml -e COMPONENT=rabbitmq -e ENV=dev"
+    ]
+  }
+}
